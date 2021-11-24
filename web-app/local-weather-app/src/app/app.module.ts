1311ee59e895696a83889f105907859003badfeb
Chuks Akibot <juniorakibor@yahoo.com>
2021-11-24 13:42:12 +0100

    crafting UI elements using components and interfaces

--- web-app/local-weather-app/src/app/app.module.ts
+++ web-app/local-weather-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CurrentWeatherComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
